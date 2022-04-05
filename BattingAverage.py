526cb9a0b75f741cc4eaa9c24d7bf08c896d877f
SeanGuirola <77602077+SeanGuirola@users.noreply.github.com>
2022-04-05 18:20:18 -0500

    Add files via upload
    an extra credit project

---
+++ BattingAverage.py
@@ -0,0 +1,5 @@
+
+atBat = float(input("Enter the player's number of times at bat: "))
+hits = float(input ("Enter the player's number of hits: "))
+battingAvg = hits / atBat
+print ("The player's batting average is ", battingAvg)
